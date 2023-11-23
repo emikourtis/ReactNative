7d302b6a28be37decdc956198b64d140090df6e5
emiliano kourtis <emilianokourtis88@gmail.com>
2023-11-23 08:56:54 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputcontainer}>
+        <TextInput style={styles.input} placeholder='producto' />
+        <Button title='AGREGAR' />
+      </View>
+      <View style={styles.listContainer} >
+        <View style={styles.cardProduct} >
+          <Text>Producto 1</Text>
+          <Text>$1200</Text>
+          <Button title='Eliminar'/>
+        </View>
+        <View style={styles.cardProduct} >
+          <Text>Producto 1</Text>
+          <Text>$1200</Text>
+          <Button title='Eliminar'/>
+        </View>
+        <View style={styles.cardProduct} >
+          <Text>Producto 1</Text>
+          <Text>$1200</Text>
+          <Button title='Eliminar'/>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    marginTop: "22",
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
+    flex: 1
+
   },
+  inputcontainer: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+    marginTop: 35
+  },
+  input: {
+    borderWidth: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: 350
+  },
+  listContainer: {
+    backgroundColor:"grey",
+    width:"100%",
+    marginTop: 25,
+    padding: 10,
+    
+  },
+  cardProduct: {
+    backgroundColor: "white",
+    flexDirection:'row',
+    padding: 15,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-between"
+  }
 });
+
