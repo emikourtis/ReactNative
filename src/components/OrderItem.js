4a4179a272c3685fd14f2e97c076f5ce5a728cb3
emiliano kourtis <emilianokourtis88@gmail.com>
2023-12-23 21:25:00 -0300

    arreglo order

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,21 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
-import Feather from '@expo/vector-icons'
+import {Feather} from '@expo/vector-icons'
+import { colors } from '../Global/colors'
 
 const OrderItem = ({ order }) => {
     const total = order.items
         .reduce((acc, product)=>acc + (product.price * product.quantity),0)
     return (
-        <View>
-            <View>
-                <Text>{new Date(order.createdAd).toLocaleString()}</Text>
-                <Text>total: {total} </Text>
-
+        <View style={styles.container} >
+            <View style={styles.containerCarts}>
+                <Text style={styles.text}>Date: {new Date(order.createdAd).toLocaleString()}</Text>
+                <Text style={styles.text}>Total: ${total} </Text>
+                <Feather name='search' size={25} color="white" />
             </View>
-            <Feather name='search' size={25} color="black" />
+            
         </View>
     )
 }
 
 export default OrderItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        width:"90%",
+        flexDirection:"row",
+        gap:50,
+        justifyContent:"space-between",
+        margin:20,
+        padding:10,
+    },
+    containerCarts:{
+        backgroundColor:colors.blue,
+        width:"100%",
+        padding:15,
+        gap:10,
+        borderRadius:20
+    },
+    text:{
+        fontFamily:"Josefin",
+        fontSize:16,
+        color:"white"
+    }
+})
