f6e52898b1801a2a7708381241b293c7b3a41707
emiliano <emi_kourtis@hotmail.com>
2024-01-25 10:17:22 -0300

    arreglos orders

--- src/screens/Orders.js
+++ src/screens/Orders.js
@@ -1,18 +1,27 @@
-import { FlatList, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import OrderItem from '../components/OrderItem'
 import { useGetOrdersQuery } from '../app/services/shopServices'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
 
 const Orders = () => {
-  const {data, isSuccess, isError, error} = useGetOrdersQuery(localId)
+  const {data, isSuccess, isError, error,isLoading} = useGetOrdersQuery(localId)
   const localId = useSelector(state=>state.auth.value.localId)
+  const [info,setInfo] = useState(true)
+  const [errorMessage,setErrorMessage] = useState("")
+  const [loading,setLoading] = useState(true)
 
   useEffect(() => {
-    if(isSuccess && data){
-      console.log(data)
+    if(isSuccess && data.lenght === 0){
+      setInfo(false)
     }
-    if(isError) console.log(error)
+    if(isSuccess && data) setLoading(false)
+    if(isError && error) setErrorMessage(error.error)
   }, [isSuccess,isError, error, data])
+
+  if(!info) return <View><Text>No tienes ordenes</Text></View>
+  if(errorMessage) return <View><Text>{errorMessage}</Text></View>
+  if(loading) return <View><Text>cargando...</Text></View>
   
   return (
     <FlatList
